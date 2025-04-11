62398b5f1b51a9f8f459421b6c16aaa12f9bbe7a
DTrombett <d@trombett.org>
2025-04-11 18:32:56 +0200

    feat: add merge images script

---
+++ src/mergeImages.ts
@@ -0,0 +1,103 @@
+// Merge images using ffmpeg
+import { execFile } from "node:child_process";
+import { homedir } from "node:os";
+import { join, parse, type ParsedPath } from "node:path";
+import { stderr, stdin, stdout } from "node:process";
+import { createInterface } from "node:readline/promises";
+import { promisify } from "node:util";
+import { getUserChoice } from "./utils/getUserChoice.ts";
+
+// Initialize the readline interface
+const rl = createInterface(stdin, stdout);
+const images: ({ width: number; height: number; path: string } & ParsedPath)[] =
+	[];
+const exec = promisify(execFile);
+const ffmpeg = exec.bind(null, "ffmpeg");
+const ffprobe = exec.bind(null, "ffprobe");
+let imagePath: string;
+
+// Keep asking for image paths until user enters nothing
+while (
+	(imagePath = (
+		await rl.question(
+			`Enter path to image (ex. C:\\Users\\acer\\Downloads\\image.png, example.png): `
+		)
+	).trim())
+) {
+	stdout.write(`\x1b[2mProcessing ${imagePath}...\x1b[0m`);
+	const result = await ffprobe(
+		[
+			imagePath,
+			"-v",
+			"error",
+			"-show_entries",
+			"stream=width,height",
+			"-of",
+			"json",
+		],
+		{ timeout: 10_000, encoding: "utf-8" }
+	);
+	stdout.write("\r\x1b[K");
+	if (result.stderr) {
+		stderr.write(result.stderr);
+		continue;
+	}
+	const parsed: { streams?: { width: number; height: number }[] } = JSON.parse(
+		result.stdout.toString()
+	);
+	if (parsed.streams?.length !== 1) {
+		stderr.write(
+			`\x1b[31mInvalid number of streams: ${parsed.streams?.length}\x1b[0m\n`
+		);
+		continue;
+	}
+	images.push({ ...parsed.streams[0]!, ...parse(imagePath), path: imagePath });
+}
+stdout.write("\x1b[A\x1b[K");
+// Prompt the user for the merge direction
+const direction = await getUserChoice("Merge direction", [
+	{
+		label: "Horizontal",
+		value: "h",
+	},
+	{
+		label: "Vertical",
+		value: "v",
+	},
+]);
+// Scale the images to the same size
+let scale = Math.max(
+	...images.map(
+		direction === "h" ? ({ height }) => height : ({ width }) => width
+	)
+).toString();
+scale = direction === "h" ? `-1:${scale}` : `${scale}:-1`;
+// Prompt the user for the path
+// Save to the downloads folder by default
+const defaultPath = join(
+	homedir(),
+	"Downloads",
+	`${images.map(({ name }) => name).join("+")}.png`
+);
+const path =
+	(await rl.question(`Output file name or path (${defaultPath}): `)) ||
+	defaultPath;
+const ffmpegArgs = [
+	"-v",
+	"error",
+	...images.flatMap(({ path }) => ["-i", path]),
+	"-filter_complex",
+	`${images
+		.map(({}, i) => `[${i}:v]scale=${scale}[${String.fromCharCode(97 + i)}];`)
+		.join(" ")} ${images
+		.map(({}, i) => `[${String.fromCharCode(97 + i)}]`)
+		.join("")}${direction}stack=inputs=${images.length}`,
+	"-y",
+	path,
+];
+// Run ffmpeg
+stdout.write(`Merging images...\n`);
+const result = await ffmpeg(ffmpegArgs, { timeout: 10_000, encoding: "utf-8" });
+if (result.stderr) stderr.write(result.stderr);
+else stdout.write(`\x1b[32mMerged images to ${path}\x1b[0m\n`);
+rl.close();
